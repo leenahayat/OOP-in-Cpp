cc5de69a83e80a77773ba58304b532760dada3c0
Leena Hayat <leenahayat14@gmail.com>
2025-08-28 22:35:05 +0500

    Create Functionsinsideclass.cpp

---
+++ Functionsinsideclass.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Cricketer{
+ public:   
+    string name;
+    int runs;
+    float avg;
+
+    Cricketer(string name, int runs, float avg){
+        this->name = name;  //points to the current object instance on which a member function is being called. 
+        this->runs = runs;
+        this->avg = avg;
+    }  
+
+    void print(int runs)//parametarized function inside class
+    {
+     cout << this->name << "   " << this->runs << "  " << this->avg << endl;
+    }
+};
+
+int main(){ 
+  Cricketer c1("Babar Azam", 25000, 57.5);
+  Cricketer c2("Muhammad Rizwan", 15000, 27.9);
+  cout << "Player" << "   " << "Runs" << "  " << "Average" << endl;
+  c1.print(5); //calling function
+  c2.print(3);
+}
